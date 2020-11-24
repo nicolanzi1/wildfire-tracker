5396472079a298fab6ab433d6d8fe1c633b925b6
nicolanzi1 <nicolanzi@gmail.com>
2020-11-24 18:23:29 +0000

    Added a LocationInfoBox to display id and title of the event, added it to the Map component and styled it

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
 const dotenv = require('dotenv')
 
 console.log(process.env)
@@ -7,12 +9,15 @@ console.log(process.env)
 dotenv.config()
 
 const Map = ({ eventData, center, zoom }) => {
+  const [locationInfo, setLocationInfo] = useState(null)
+
   const markers = eventData.map((ev) => {
     if (ev.categories[0].id === 8) {
       return (
         <LocationMarker
           lat={ev.geometries[0].coordinates[1]}
           lng={ev.geometries[0].coordinates[0]}
+          onClick={() => setLocationInfo({ id: ev.id, title: ev.title })}
         />
       )
     }
@@ -28,6 +33,7 @@ const Map = ({ eventData, center, zoom }) => {
       >
         {markers}
       </GoogleMapReact>
+      {locationInfo && <LocationInfoBox info={locationInfo} />}
     </div>
   )
 }
